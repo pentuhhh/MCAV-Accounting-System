4ed0881d147dfe35379a43a7f2e6eb1511d9cae4
Ron Patrick Ramas <143059096+MrChooey@users.noreply.github.com>
2024-06-26 11:22:11 +0800

    Update index.php
    Added: html doc
    Added: router

--- src/public/index.php
+++ src/public/index.php
@@ -1,3 +1,11 @@
+<?php
+
+const BASE_PATH = __DIR__ . "/..";
+
+$urlPath = parse_url($_SERVER["REQUEST_URI"], PHP_URL_PATH);
+$pagePath = BASE_PATH . "/pages$urlPath";
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -6,6 +14,16 @@
     <title>MCAV</title>
 </head>
 <body>
-    
+    <?php
+    if (file_exists($pagePath . "/index.php")) {
+        include_once $pagePath . "/index.php";
+    } elseif (file_exists($pagePath . ".php")) {
+        include_once $pagePath . ".php";
+    } else {
+        // TODO: Improve 404 error page.
+        http_response_code(404);
+        echo "404 Not Found";
+    }
+    ?>
 </body>
 </html>
