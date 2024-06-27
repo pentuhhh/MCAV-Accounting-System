6fab83ac14cb6834548143f8fd6e0fa50e987909
MrChooey <ronpatrickramas7@gmail.com>
2024-06-27 16:10:55 +0800

    Create receipts

---
+++ src/pages/receipts/index.php
@@ -0,0 +1,12 @@
+<div class="flex">
+    <?php
+    include_once __DIR__ . "/../../components/sidebar.php";
+    ?>
+
+    <div class="flex items-center w-dvw max-h-[100px] p-8">
+        <i class="material-symbols-rounded text-[42px]">
+            payments
+        </i>
+        <span class="pl-2 text-2xl font-bold">Receipts</span>
+    </div>
+</div>
