94bb968ee07fe6dd499fdcf26706495784dce46e
MrChooey <ronpatrickramas7@gmail.com>
2024-06-27 16:10:42 +0800

    Create orders

---
+++ src/pages/orders/index.php
@@ -0,0 +1,12 @@
+<div class="flex">
+    <?php
+    include_once __DIR__ . "/../../components/sidebar.php";
+    ?>
+
+    <div class="flex items-center w-dvw max-h-[100px] p-8">
+        <i class="material-symbols-rounded text-[42px]">
+            receipt_long
+        </i>
+        <span class="pl-2 text-2xl font-bold">Orders</span>
+    </div>
+</div>
