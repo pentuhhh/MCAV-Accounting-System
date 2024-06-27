6af29da4ffde09d3442c83b703df9d0c4aa700ea
MrChooey <ronpatrickramas7@gmail.com>
2024-06-27 16:10:12 +0800

    Updated dashboard
    Added: Header

--- src/pages/dashboard/index.php
+++ src/pages/dashboard/index.php
@@ -2,4 +2,19 @@
     <?php
     include_once __DIR__ . "/../../components/sidebar.php";
     ?>
+
+    <!-- Header -->
+    <div class="flex items-center w-dvw max-h-[100px] p-8">
+        <i class="material-symbols-rounded text-[42px]">
+            dashboard
+        </i>
+        <span class="pl-2 text-2xl font-bold">Dashboard</span>
+    </div>
+    <!-- End of Header -->
+
+    <!-- Analytics -->
+    <div class="block items-center w-dvw max-h-[100px] p-8">
+        <h1 class="text-2xl font-bold">Analytics</h1>
+    </div>
+    <!-- End of Analytics -->
 </div>
