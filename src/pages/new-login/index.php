76760542ebd098e862f1bc5306dbf7e55f8e3de6
MrChooey <ronpatrickramas7@gmail.com>
2024-06-27 16:10:27 +0800

    Create new-login

---
+++ src/pages/new-login/index.php
@@ -0,0 +1,12 @@
+<div class="flex">
+    <?php
+    include_once __DIR__ . "/../../components/sidebar.php";
+    ?>
+
+    <div class="flex items-center w-dvw max-h-[100px] p-8">
+        <i class="material-symbols-rounded text-[42px]">
+            person
+        </i>
+        <span class="pl-2 text-2xl font-bold">Add New User</span>
+    </div>
+</div>
