42091fe68c5ab08636c9ab3a086e10b2f8a02675
Ron Patrick Ramas <143059096+MrChooey@users.noreply.github.com>
2024-07-02 16:20:43 +0800

    Updated src/pages/dashboard
    Added: Page skeleton (analytics, recent orders table)

--- src/pages/dashboard/index.php
+++ src/pages/dashboard/index.php
@@ -1,20 +1,62 @@
-<div class="flex">
+<div class="flex w- full">
     <?php
     include_once __DIR__ . "/../../components/sidebar.php";
     ?>
 
-    <!-- Header -->
-    <div class="flex items-center w-dvw max-h-[100px] p-8">
-        <i class="material-symbols-rounded text-[42px]">
-            dashboard
-        </i>
-        <span class="pl-2 text-2xl font-bold">Dashboard</span>
-    </div>
-    <!-- End of Header -->
+    <div class="flex flex-col">
+        <!-- Header -->
+        <div class="flex items-center p-8">
+            <i class="material-symbols-rounded text-[42px]">
+                dashboard
+            </i>
+            <span class="pl-2 text-2xl font-bold">Dashboard</span>
+        </div>
+        <!-- End of Header -->
+    
+        <!-- Analytics -->
+        <div class="items-center px-8 pb-4">
+            <h1 class="text-2xl font-bold">Analytics</h1>
+        </div>
+    
+        <div class="flex px-8">
+            <!-- Monthly Sales -->
+            <div class="column-1">
+                <h3>Monthly Sales</h3>
+            </div>
+            <!-- Total Orders -->
+            <div class="column-2">
+                <h3>Total Orders</h3>
+            </div>
+            <!-- Total Sales -->
+            <div class="column-3">
+                <h3>Total Sales</h3>
+            </div>
+        </div>
+        <!-- End of Analytics -->
 
-    <!-- Analytics -->
-    <div class="block items-center w-dvw max-h-[100px] p-8">
-        <h1 class="text-2xl font-bold">Analytics</h1>
+        <!-- Recent Orders -->
+        <div class="items-center px-8">
+            <h1 class="text-2xl font-bold">Recent Orders</h1>
+            <div>
+                <table>
+                    <tr>
+                        <th>Order ID</th>
+                        <th>Customer</th>
+                        <th>Product Description</th>
+                        <th>Qty</th>
+                        <th>Order Date</th>
+                        <th>Amount</th>
+                        <th>Date Released</th>
+                        <th>Status</th>
+                        <th>Remarks</th>
+                    </tr>
+                    <tr>
+                        <td></td>
+                    </tr>
+                </table>
+                <a href="">Show all</a>
+            </div>
+        </div>
+        <!-- End of Recent Orders -->
     </div>
-    <!-- End of Analytics -->
 </div>
