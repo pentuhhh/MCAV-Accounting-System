63f94934f2f63a2d9d93b45cf9405392582d311a
Ron Patrick Ramas <143059096+MrChooey@users.noreply.github.com>
2024-07-04 16:35:25 +0800

    Updated src/pages/users
    Added: Front end designs

--- src/pages/users/index.php
+++ src/pages/users/index.php
@@ -1,12 +1,53 @@
-<div class="flex">
+<div class="GLOBAL_PAGE">
     <?php
     include_once __DIR__ . "/../../components/sidebar.php";
     ?>
 
-    <div class="flex items-center w-dvw max-h-[100px] p-8">
-        <i class="material-symbols-rounded text-[42px]">
-            person
-        </i>
-        <span class="pl-2 text-2xl font-bold">Users</span>
+    <div class="GLOBAL_PAGE_CONTAINER">
+        <div class="GLOBAL_HEADER">
+            <div class="GLOBAL_HEADER_TITLE">
+                <i class="material-symbols-rounded text-[42px]">
+                    Person
+                </i>
+                <span>Users</span>
+            </div>
+            <div>
+                <div class="GLOBAL_HEADER_COLUMN">
+                    <p>Hey, <strong>Radon</strong></p>
+                    <p>Admin</p>
+                </div>
+            </div>
+        </div>
+    
+        <div class="ORDERS_SEARCH">
+            <div class="columns-1">
+                <a href="">
+                    <i class="material-symbols-rounded">
+                        search
+                    </i>
+                </a>
+                <input type="text" placeholder="Search">
+            </div>
+        </div>
+    
+        <div class="ORDERS_CONTENT">
+            <div class="GLOBAL_TABLE">
+                <table>
+                    <tr>
+                        <th>#</th>
+                        <th>User</th>
+                        <th>Last Name</th>
+                        <th>First Name</th>
+                        <th>Email</th>
+                        <th>Contact Number</th>
+                        <th>Position</th>
+                        <th></th>
+                    </tr>
+                    <tr>
+                        <td></td>
+                    </tr>
+                </table>
+            </div>
+        </div>
     </div>
 </div>
