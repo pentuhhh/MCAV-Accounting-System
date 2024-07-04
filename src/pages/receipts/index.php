53ec1d990cf1c4d12e7fee2e12a6cb9c47bc30f8
Ron Patrick Ramas <143059096+MrChooey@users.noreply.github.com>
2024-07-04 16:34:51 +0800

    Updated src/pages/receipts
    Added: Front end designs

--- src/pages/receipts/index.php
+++ src/pages/receipts/index.php
@@ -1,12 +1,69 @@
-<div class="flex">
+<div class="GLOBAL_PAGE">
     <?php
     include_once __DIR__ . "/../../components/sidebar.php";
     ?>
 
-    <div class="flex items-center w-dvw max-h-[100px] p-8">
-        <i class="material-symbols-rounded text-[42px]">
-            payments
-        </i>
-        <span class="pl-2 text-2xl font-bold">Receipts</span>
+    <div class="GLOBAL_PAGE_CONTAINER">
+        <div class="GLOBAL_HEADER">
+            <div class="GLOBAL_HEADER_TITLE">
+                <i class="material-symbols-rounded text-[42px]">
+                    payments
+                </i>
+                <span>Receipts</span>
+            </div>
+            <div>
+                <div class="GLOBAL_HEADER_COLUMN">
+                    <p>Hey, <strong>Radon</strong></p>
+                    <p>Admin</p>
+                </div>
+            </div>
+        </div>
+    
+        <div class="DASHBOARD_ANALYTICS">
+            <div class="DASHBOARD_ANALYTICS_ROW">
+                <div class="DASHBOARD_ANALYTICS_CARD GLOBAL_BOX_DIV">
+                    <h1 class="DASHBOARD_ANALYTICS_CARD_TITLE">Last Month's Income</h1></h1>
+                    <h1 class="DASHBOARD_ANALYTICS_CARD_VALUE">Php 65,521+</h1>
+                </div>
+                <div class="DASHBOARD_ANALYTICS_CARD GLOBAL_BOX_DIV">
+                    <h1 class="DASHBOARD_ANALYTICS_CARD_TITLE">This Month's Income</h1>
+                    <h1 class="DASHBOARD_ANALYTICS_CARD_VALUE">Php 48,200+</h1>
+                </div>
+                <div class="DASHBOARD_ANALYTICS_CARD GLOBAL_BOX_DIV">
+                    <h1 class="DASHBOARD_ANALYTICS_CARD_TITLE">Total Sales</h1>
+                    <h1 class="DASHBOARD_ANALYTICS_CARD_VALUE">Php 48,156+</h1>
+                </div>
+            </div>
+        </div>
+
+        <div class="ORDERS_SEARCH">
+            <div class="columns-1">
+                <a href="">
+                    <i class="material-symbols-rounded">
+                        search
+                    </i>
+                </a>
+                <input type="text" placeholder="Search">
+            </div>
+        </div>
+    
+        <div class="ORDERS_CONTENT">
+            <div class="GLOBAL_TABLE">
+                <table>
+                    <tr>
+                        <th>#</th>
+                        <th>Receipt</th>
+                        <th>Order</th>
+                        <th>Payment Method</th>
+                        <th>Amount Paid</th>
+                        <th>Payment Date</th>
+                        <th></th>
+                    </tr>
+                    <tr>
+                        <td></td>
+                    </tr>
+                </table>
+            </div>
+        </div>
     </div>
 </div>
