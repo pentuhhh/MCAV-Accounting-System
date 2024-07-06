14ef01d8766fa96d74b9df0d1aa1a83116b7d802
Ron Patrick Ramas <143059096+MrChooey@users.noreply.github.com>
2024-07-06 12:49:07 +0800

    Create src/pages/orders/add-new/products

---
+++ src/pages/orders/add-new/products/index.php
@@ -0,0 +1,35 @@
+<div class="GLOBAL_PAGE">
+	<?php
+    include_once __DIR__ . "/../../../../components/sidebar.php";
+    ?>
+
+	<div class="GLOBAL_PAGE_CONTAINER">
+        <div class="GLOBAL_HEADER">
+            <div class="GLOBAL_HEADER_TITLE">
+                <i class="material-symbols-rounded text-[42px]">
+                    receipt_long
+                </i>
+                <span class="">Add New Order</span>
+            </div>
+            <div class="GLOBAL_HEADER_USER">
+                <div class="GLOBAL_HEADER_COLUMN">
+                    <p class="text-sm text-[#7F7F7F]">Hey, <strong class="text-black">Radon</strong></p>
+                    <p class="text-sm text-[#7F7F7F]">Admin</p>
+                </div>
+                <img src="/assets/JumanjiRon.png" alt="">
+            </div>
+        </div>
+
+        <div class="px-10 flex-grow">
+            <div class="flex flex-row pb-5 items-center justify-between">
+                <h1 class="text-2xl font-bold">User Information</h1> 
+                <a onclick="window.history.back(); return false;" class="flex items-center outline-none">
+                    <i class="material-symbols-rounded text-[#7F7F7F] text-3xl">
+                        arrow_back
+                    </i>
+                </a>
+            </div>
+        </div>
+    </div>
+
+</div>
