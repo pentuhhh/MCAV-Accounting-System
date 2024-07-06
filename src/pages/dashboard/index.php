1177587f0fc5a39fd2be5460482135e5325b36a4
Ron Patrick Ramas <143059096+MrChooey@users.noreply.github.com>
2024-07-06 12:47:26 +0800

    Updated src/pages/dashboard
    Added: updated CSS styles

--- src/pages/dashboard/index.php
+++ src/pages/dashboard/index.php
@@ -11,58 +11,54 @@
                 </i>
                 <span>Dashboard</span>
             </div>
-            <div>
+            <div class="GLOBAL_HEADER_USER">
                 <div class="GLOBAL_HEADER_COLUMN">
                     <p class="text-sm text-[#7F7F7F]">Hey, <strong class="text-black">Radon</strong></p>
                     <p class="text-sm text-[#7F7F7F]">Admin</p>
                 </div>
+                <img src="/assets/JumanjiRon.png" alt="">
             </div>
         </div>
 
-        <div class="DASHBOARD_ANALYTICS">
-            <div class="DASHBOARD_ANALYTICS_HEADER">
-                <h1 class="DASHBOARD_ANALYTICS_HEADER_TITLE">Analytics</h1> 
-                <a href="">
-                    <i class="material-symbols-rounded text-[#7F7F7F] text-3xl">
-                        arrow_back
-                    </i>
-                </a>
+        <div class="GLOBAL_ANALYTICS">
+            <div class="GLOBAL_SUBHEADER">
+                <h1 class="GLOBAL_SUBHEADER_TITLE">Analytics</h1> 
             </div>
-            <div class="DASHBOARD_ANALYTICS_ROW">
-                <div class="DASHBOARD_ANALYTICS_CARD GLOBAL_BOX_DIV">
-                    <h1 class="DASHBOARD_ANALYTICS_CARD_TITLE">Monthly Sales</h1>
-                    <h1 class="DASHBOARD_ANALYTICS_CARD_VALUE">Php 65,521</h1>
+            <div class="GLOBAL_ANALYTICS_ROW">
+                <div class="GLOBAL_ANALYTICS_CARD GLOBAL_BOX_DIV">
+                    <h1 class="GLOBAL_ANALYTICS_CARD_TITLE">Monthly Sales</h1>
+                    <h1 class="GLOBAL_ANALYTICS_CARD_VALUE">Php 65,521</h1>
                 </div>
-                <div class="DASHBOARD_ANALYTICS_CARD GLOBAL_BOX_DIV">
-                    <h1 class="DASHBOARD_ANALYTICS_CARD_TITLE">Total Orders</h1>
-                    <h1 class="DASHBOARD_ANALYTICS_CARD_VALUE">1,002</h1>
+                <div class="GLOBAL_ANALYTICS_CARD GLOBAL_BOX_DIV">
+                    <h1 class="GLOBAL_ANALYTICS_CARD_TITLE">Total Orders</h1>
+                    <h1 class="GLOBAL_ANALYTICS_CARD_VALUE">1,002</h1>
                 </div>
-                <div class="DASHBOARD_ANALYTICS_CARD GLOBAL_BOX_DIV">
-                    <h1 class="DASHBOARD_ANALYTICS_CARD_TITLE">Total Sales</h1>
-                    <h1 class="DASHBOARD_ANALYTICS_CARD_VALUE">Php 105,156</h1>
+                <div class="GLOBAL_ANALYTICS_CARD GLOBAL_BOX_DIV">
+                    <h1 class="GLOBAL_ANALYTICS_CARD_TITLE">Total Sales</h1>
+                    <h1 class="GLOBAL_ANALYTICS_CARD_VALUE">Php 105,156</h1>
                 </div>
             </div>
         </div>
     
         <div class="DASHBOARD_RECENT">
-            <h1 class="DASHBOARD_RECENT_HEADER">Recent Orders</h1>
+            <h1 class="GLOBAL_SUBHEADER_TITLE">Recent Orders</h1>
             <div class="GLOBAL_TABLE">
                 <table>
                     <tr>
-                        <th>#</th>
-                        <th>Customer</th>
-                        <th>Order Date</th>
-                        <th>Amount</th>
-                        <th>Date Released</th>
-                        <th>Status</th>
-                        <th>Remarks</th>
+                        <th><a href="">#</a></th>
+                        <th><a href="">Customer</a></th>
+                        <th><a href="">Order Date</a></th>
+                        <th><a href="">Amount</a></th>
+                        <th><a href="">Date Released</a></th>
+                        <th><a href="">Status</a></th>
+                        <th><a href="">Remarks</a></th>
                     </tr>
                     <tr>
                         <td></td>
                     </tr>
                 </table>
             </div>
-            <a href="">Show all</a>
+            <a href="" class="DASHBOARD_RECENT_SHOW_A">Show all</a>
         </div>
     </div>
 </div>
