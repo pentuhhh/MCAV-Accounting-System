93e4ece1000a2ecbb4eaa6dbebd8fd9e24b6f64b
alphas12 <146078143+alphas12@users.noreply.github.com>
2024-07-11 15:23:26 +0800

    n

--- src/public/index.php
+++ src/public/index.php
@@ -1,31 +1,40 @@
 <?php
 
-const BASE_PATH = __DIR__ . "/..";
+const BASE_PATH = __DIR__ . "/../..";
 
 $urlPath = parse_url($_SERVER["REQUEST_URI"], PHP_URL_PATH);
-$pagePath = BASE_PATH . "/pages$urlPath";
+
+session_start();
+
+$pagePath = BASE_PATH . "/src/pages$urlPath";
+
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>MCAV</title>
     <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Rounded:wght,FILL@300,1" />
-    <link type="text/css" rel="stylesheet" href="/output.css"/>
+    <link type="text/css" rel="stylesheet" href="/output.css" />
+    <script src="/script.js" defer></script>
 </head>
+
 <body class="font-poppins">
     <?php
+    // Include the requested page if it exists
     if (file_exists($pagePath . "/index.php")) {
         include_once $pagePath . "/index.php";
     } elseif (file_exists($pagePath . ".php")) {
         include_once $pagePath . ".php";
     } else {
-        // TODO: Improve 404 error page.
+        // Show a 404 error if the page is not found
         http_response_code(404);
         echo "404 Not Found";
     }
     ?>
 </body>
+
 </html>
