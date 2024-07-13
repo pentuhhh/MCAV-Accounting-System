777c5d3a5276dea03a4b6d4698b70a46025f0449
pentuhhh <firefall.challenger@gmail.com>
2024-07-14 02:00:24 +0800

    Fixed the labels and names. Added elements
    Ron the submit button needs to be the a submit type inside the form.

--- src/pages/receipts/add-receipt/index.php
+++ src/pages/receipts/add-receipt/index.php
@@ -30,24 +30,26 @@
                 </a>
             </div>
 
-            <form action="" method="post">
+            <form action="handler" method="post">
                 <div class="CUSTOMERS_INPUT GLOBAL_BOX_DIV">
-
                     <!-- Column 1 -->
                     <div class="CUSTOMERS_INPUT_COLUMN">
+                        <!-- Order ID -->
+                        <div class="CUSTOMERS_INPUT_COLUMN_CONTAINER">
+                            <label for="order-id">Order ID</label>
+                            <input type="number" id="order-id" name="order-id" min="0" placeholder="Order ID" required>
+                        </div>
                         <!-- Payment Date -->
                         <div class="CUSTOMERS_INPUT_COLUMN_CONTAINER">
                             <label for="payment-date">Payment Date</label>
                             <div class="flex flex-row gap-2">
-                                <input type="number" id="month" min="1" max="12" size="2" placeholder="Month" required>
-                                <input type="number" id="day" min="1" max="31" size="2" placeholder="Day" required>
-                                <input type="number" id="year" min="1900" max="2100" placeholder="Year" size="4" required>
+                                <input id="payment-date" name="payment-date" type="date" required>
                             </div>
                         </div>
                         <!-- Amount -->
                         <div class="CUSTOMERS_INPUT_COLUMN_CONTAINER">
                             <label for="amount">Amount</label>
-                            <input type="number" id="amount" min="0" placeholder="Amount" required>
+                            <input type="number" id="amount" name="amount" min="0" placeholder="Amount" required>
                         </div>
                     </div>
 
@@ -59,14 +61,20 @@
                             <input id="fileInput" type="file" class="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:border-blue-500 focus:ring-blue-500 sm:text-sm">
                             <p class="mt-2 text-xs text-gray-500">Only .jpg, .png, files allowed</p>
                         </div>
+                        <!-- Reference Number -->
+                        <div class="CUSTOMERS_INPUT_COLUMN_CONTAINER">
+                            <label for="reference-number">Reference Number</label>
+                            <input type="number" id="reference-number" name="reference-number" min="0" placeholder="Reference Number">
+                        </div>
                     </div>
                 </div>
 
                 <div class="flex flex-row justify-end pb-5">
                     <a href="/receipts" class="GLOBAL_BUTTON_GRAY mr-4">Cancel</a>
-                    <a href="/receipts" class="GLOBAL_BUTTON_BLUE" onclick="scrollToSection('input')">Proceed</a>
+                    <input type="submit"  value="Add Receipt" class="GLOBAL_BUTTON_BLUE">
                 </div>
             </form>
+
         </div>
     </div>
 </div>
