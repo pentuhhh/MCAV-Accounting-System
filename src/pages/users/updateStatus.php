507af3515c35b9e305291d693ba6917de5b2b901
pentuhhh <firefall.challenger@gmail.com>
2024-07-15 09:48:04 +0800

    Only admins can activate and deactivate

--- src/pages/users/updateStatus.php
+++ src/pages/users/updateStatus.php
@@ -2,14 +2,23 @@
 // delete_user.php
 require "../utilities/db-connection.php"; // Adjust the path as needed
 
+//Block Access
+
+$employeeWebID = $_SESSION['employeeWebID'];
+
+$sql = "select UserLevel from employee_credentials where employeeWebID = $employeeWebID;";
+$result = $conn->query($sql);
+$row = $result->fetch_assoc();
+$userlevel = $row['UserLevel'];
+
 if ($_SERVER["REQUEST_METHOD"] !== "POST") {
     header("Location: /receipts");
     exit();
 }
 
 require_once "../utilities/db-connection.php";
-
-$sql = <<<SQL
+if($userlevel == 1){
+    $sql = <<<SQL
         -- UPDATE employee_credentials SET AccountStatus = IF( AccountStatus = 'Deactivated', 'Activated', 'Deactivated') WHERE EmployeeWebID = {$_POST["id"]}
         UPDATE employee_credentials SET AccountStatus = CASE
         WHEN AccountStatus = 'deactivated' THEN 'Activated' 
@@ -17,9 +26,14 @@ $sql = <<<SQL
         WHERE EmployeeWebID = {$_POST["id"]}
         SQL;
 
-$result = $conn->query($sql);
-$conn->close();
+    $result = $conn->query($sql);
+    $conn->close();
+    header("Location: /users");
+} else {
+    echo "access denied";
+}
 
-header("Location: /users");
+
+
 exit();
 
